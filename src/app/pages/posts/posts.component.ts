470da96586e733b926bb7b28eb370c7a34ae7b4c
Jose Luis <pixiestux@gmail.com>
2021-01-20 17:55:39 -0600

    avances generales del repaso de angular

---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  mensajes: any[] = [];
+
+  constructor( private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getPosts()
+      .subscribe( (posts: any[]) =>{
+        console.log(posts);
+        this.mensajes = posts;
+      });
+  }
+
+}
