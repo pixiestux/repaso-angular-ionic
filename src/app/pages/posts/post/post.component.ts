c7e6178cb238f407eeb5becde3e3808006c16d82
Jose Luis <pixiestux@gmail.com>
2021-01-20 23:01:56 -0600

    Fin de la seccion de repaso

--- src/app/pages/posts/post/post.component.ts
+++ src/app/pages/posts/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter  } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,11 +7,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  @Input() mensaje;
+  @Input() mensaje;// para recibir parametros del componente padre
+  @Output() clickPost = new EventEmitter<number>();// para mandar info al componente padre de tipo NUMBER
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(){
+    this.clickPost.emit(this.mensaje.id);
   }
 
 }
